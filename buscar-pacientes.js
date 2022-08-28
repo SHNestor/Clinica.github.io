ff0865451e481edd57198a74776f9abccbc1605e
nestor sanchez <nsh1831100515@gmail.com>
2022-08-27 22:54:47 -0500

    Se agrego challenge 404

--- buscar-pacientes.js
+++ buscar-pacientes.js
@@ -1,15 +1,18 @@
 var buttonSearch = document.querySelector("#buscar-paciente");
 
 buttonSearch.addEventListener("click",function(){
+    var linkDatos = document.querySelector("#linkDatos");
     //API es un repositorio que ya esta listo 
     // al caul se puede acceder a datos accesibles
     // se pueden accerdes atraves de varios protocolos
     let xhr = new XMLHttpRequest;
     let errorAjax = document.querySelector("#error-ajax");
 
-    let linkIn = "https://alura-es-cursos.github.io/api-pacientes/pacientes.json";
+    let linkIn = linkDatos.value;
     let linkOp = "https://alura-es-cursos.github.io/api-pacientes/pacientes.json";
     
+    console.log(linkIn);
+
     if (linkIn == linkOp){
         xhr.open("GET", linkIn);
         //Escucha el el evento load, de cargar
@@ -23,8 +26,7 @@ buttonSearch.addEventListener("click",function(){
                 });
         });
     }else{
-        console.log("El else funciona");
-        errorAjax.classList.remove("invisible");
+        location.href = "paginaerror.html";
     }
     xhr.send();
 });
